89eb8af35aa4d0c77e98ff3a8177afe1fb1b17a2
trapbed <hasanova.leya@yandex.ru>
2024-10-13 22:41:58 +0500

    верстка

---
+++ frontend/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a class="c2">
+            <div class="c3 name">Имя</div>
+        </a>
+        <a class="c2">
+            <img src="images/image 2.svg" class="folder">
+            <div class="c3">Проекты</div>
+        </a>
+        <a class="c2">
+            <img src="images/image 3.svg">
+            <div class="c3">Задачи</div>
+        </a>
+        <a class="c2">
+            <img src="images/image.svg">
+            <div class="c3">Пользователи</div>
+        </a>
+        <a class="c2">
+            <img src="images/image 4.svg">
+            <div class="c3">Отчеты</div>
+        </a>
+    </nav>
---
+++ frontend/index.php
@@ -0,0 +1,34 @@
+<?php require "header.php"; ?>
+
+<div class="sf">
+    <input type="text" class="c3-5 r1" name="search" placeholder="поиск проектов..">
+    <img src="images/image 11.svg" class="c4 r1 search_icon">
+    <select class="c6 r1" name="filter">
+        <option>все</option>
+        <option>создан</option>
+        <option>в процессе</option>
+        <option>завершен</option>
+    </select>
+</div>
+<main>
+    <div class="c3 headline">
+        <h2>Мои проекты</h2>
+    </div>
+    <div class="c3 block_project">
+       <img src="images/image 2.svg" class="c2 r1 project_folder">
+       <div class="c4 r1 project_name">Проект 1</div>
+       <div class="c5 r1 circle_gray"></div>
+       <div class="c7 r1 project_boss">
+            <img src="images/people 1.svg" class="c1 r1">
+            <div class="c3 r1">Имя</div>
+       </div>
+       <div class="c6 r1">завершен</div>
+       <div class="c9-all r1">до 22.10.2024</div>
+       <div class="c2-8 r2-all project_desc">Описание проекта................................<br>...................<br>.................................................................................................................................................................................................................................</div>
+       <img src="images/pen (1).svg" class="c10 r2-all pen">
+       <img src="images/delete 3.svg" class="c11 r2-all trash">
+    </div>
+</main>
+
+</body>
+</html>
