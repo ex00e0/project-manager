709056a47ef6f5dade2c4998ea39065a043fd9ee
trapbed <hasanova.leya@yandex.ru>
2024-10-22 06:38:43 +0500

    перенос

--- frontend/index.php
+++ frontend/index.php
@@ -28,6 +28,38 @@
        <img src="images/pen (1).svg" class="c10 r2-all pen">
        <img src="images/delete 3.svg" class="c11 r2-all trash">
     </div>
+    <div class="vh1-1 c3"></div>
+
+    <div class="c3 block_project">
+       <img src="images/image 2.svg" class="c2 r1 project_folder">
+       <div class="c4 r1 project_name">Проект 1</div>
+       <div class="c5 r1 circle_green"></div>
+       <div class="c7 r1 project_boss">
+            <img src="images/people 1.svg" class="c1 r1">
+            <div class="c3 r1">Имя</div>
+       </div>
+       <div class="c6 r1">в процессе</div>
+       <div class="c9-all r1">до 22.10.2024</div>
+       <div class="c2-8 r2-all project_desc">Описание проекта................................<br>...................<br>.................................................................................................................................................................................................................................</div>
+       <img src="images/pen (1).svg" class="c10 r2-all pen">
+       <img src="images/delete 3.svg" class="c11 r2-all trash">
+    </div>
+    <div class="vh1-1 c3"></div>
+
+    <div class="c3 block_project">
+       <img src="images/image 2.svg" class="c2 r1 project_folder">
+       <div class="c4 r1 project_name">Проект 1</div>
+       <div class="c5 r1 circle_yellow"></div>
+       <div class="c7 r1 project_boss">
+            <img src="images/people 1.svg" class="c1 r1">
+            <div class="c3 r1">Имя</div>
+       </div>
+       <div class="c6 r1">создан</div>
+       <div class="c9-all r1">до 22.10.2024</div>
+       <div class="c2-8 r2-all project_desc">Описание проекта................................<br>...................<br>.................................................................................................................................................................................................................................</div>
+       <img src="images/pen (1).svg" class="c10 r2-all pen">
+       <img src="images/delete 3.svg" class="c11 r2-all trash">
+    </div>
 </main>
 
 </body>
