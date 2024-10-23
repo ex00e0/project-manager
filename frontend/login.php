307fc4d704ab8829c5695318f5980bc7fef328b4
trapbed <hasanova.leya@yandex.ru>
2024-10-23 20:44:53 +0500

    вход

---
+++ frontend/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>project manager</title>
+</head>
+<body>
+    <main>
+        <div class="shadow"></div>
+        <form class="modal_auth">
+            <div class="c1 r1 headline_modal">Вход</div>
+            <input type="email" class="c1 r2" placeholder="электронная почта">
+            <input type="password" class="c1 r4" placeholder="пароль">
+            <input type="submit" class="c1 r5" value="войти">
+        </form>
+    </main>
+</body>
+</html>
+
