b5e622b42e882a2bf516189e93e921f5293802e7
trapbed <hasanova.leya@yandex.ru>
2024-10-23 13:43:34 +0500

    add laravel

--- frontend/tasks.php
+++ frontend/tasks.php
@@ -21,6 +21,8 @@
         <div>Приоритет</div>
         <div>Срок</div>
         <div>Статус</div>
+        <div></div>
+        <div></div>
     </div>
     <div class="c3 tr">
         <div>Задача 1</div>
@@ -34,12 +36,72 @@
         </div>
         <div class="double">
             <img src="images/Group 1.svg">
-            <div>Высокий</div>
+            <div>высокий</div>
         </div>
         <div>до 22.10.2024</div>
-        <div class="double">
+        <div class="double2">
             <div class="circle_gray js_c"></div>
             <div>завершена</div>
+        </div>
+        <div class="except">
+            <img src="images/pen (1).svg" class="pen">
+        </div>
+        <div class="except">
+            <img src="images/delete 3.svg" class="trash">
+        </div>
+    </div>
+
+    <div class="c3 tr">
+        <div>Задача 1</div>
+        <div class="double">
+            <img src="images/people 4.svg">
+            <div>Имя</div>
+        </div>
+        <div class="double">
+            <img src="images/people 4.svg">
+            <div>Имя</div>
+        </div>
+        <div class="double">
+            <img src="images/Group 2.svg" class="midP">
+            <div>средний</div>
+        </div>
+        <div>до 22.10.2024</div>
+        <div class="double2">
+            <div class="circle_green js_c"></div>
+            <div>выполняется</div>
+        </div>
+        <div class="except">
+            <img src="images/pen (1).svg" class="pen">
+        </div>
+        <div class="except">
+            <img src="images/delete 3.svg" class="trash">
+        </div>
+    </div>
+
+    <div class="c3 tr last_tr">
+        <div>Задача 1</div>
+        <div class="double">
+            <img src="images/people 4.svg">
+            <div>Имя</div>
+        </div>
+        <div class="double">
+            <img src="images/people 4.svg">
+            <div>Имя</div>
+        </div>
+        <div class="double">
+            <img src="images/Vector 7.svg" class="lowP">
+            <div>низкий</div>
+        </div>
+        <div>до 22.10.2024</div>
+        <div class="double2">
+            <div class="circle_yellow js_c"></div>
+            <div>назначена</div>
+        </div>
+        <div class="except">
+            <img src="images/pen (1).svg" class="pen">
+        </div>
+        <div class="except">
+            <img src="images/delete 3.svg" class="trash">
         </div>
     </div>
 </main>
